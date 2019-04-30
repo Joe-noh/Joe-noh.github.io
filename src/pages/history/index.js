5793e314f7f61948d8eba9ebf1bdfe5e548d8d0d
Joe-noh <goflb.jh@gmail.com>
2019-04-30 22:40:52 +0900

    用意したcomponentで書こうね

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -2,32 +2,31 @@ import React from "react"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import Article, { ArticleHeading, Section, SectionHeading, SectionSubheading } from '../../components/article'
 import ExtLink from '../../components/ext-link'
-
-import styles from './style.module.css'
 
 const History = () => (
   <Layout>
     <SEO title="History" />
-    <article className={styles.article}>
-      <h2 className={styles.heading}>History</h2>
-      <section className={styles.section}>
-        <h3 className={styles.org}><ExtLink href="https://www.oyama-ct.ac.jp/">小山工業高等専門学校</ExtLink> 電子制御工学科</h3>
-        <span className={styles.duration}>2006/4 - 2011/3</span>
-      </section>
-      <section className={styles.section}>
-        <h3 className={styles.org}><ExtLink href="https://www.nagaokaut.ac.jp/">長岡技術科学大学</ExtLink> 電気電子情報工学課程</h3>
-        <span className={styles.duration}>2011/4 - 2013/3</span>
-      </section>
-      <section className={styles.section}>
-        <h3 className={styles.org}><ExtLink href="https://www.nagaokaut.ac.jp/">長岡技術科学大学</ExtLink> 工学研究科 電気電子情報工学専攻</h3>
-        <span className={styles.duration}>2013/4 - 2014/12 (退学)</span>
-      </section>
-      <section className={styles.section}>
-        <h3 className={styles.org}><ExtLink href="https://pepabo.com/">GMOペパボ株式会社</ExtLink></h3>
-        <span className={styles.duration}>2015/4 -</span>
-      </section>
-    </article>
+    <Article>
+      <ArticleHeading>History</ArticleHeading>
+      <Section>
+        <SectionHeading><ExtLink href="https://www.oyama-ct.ac.jp/">小山工業高等専門学校</ExtLink> 電子制御工学科</SectionHeading>
+        <SectionSubheading>2006/4 - 2011/3</SectionSubheading>
+      </Section>
+      <Section>
+        <SectionHeading><ExtLink href="https://www.nagaokaut.ac.jp/">長岡技術科学大学</ExtLink> 電気電子情報工学課程</SectionHeading>
+        <SectionSubheading>2011/4 - 2013/3</SectionSubheading>
+      </Section>
+      <Section>
+        <SectionHeading><ExtLink href="https://www.nagaokaut.ac.jp/">長岡技術科学大学</ExtLink> 工学研究科 電気電子情報工学専攻</SectionHeading>
+        <SectionSubheading>2013/4 - 2014/12 (退学)</SectionSubheading>
+      </Section>
+      <Section>
+        <SectionHeading><ExtLink href="https://pepabo.com/">GMOペパボ株式会社</ExtLink></SectionHeading>
+        <SectionSubheading>2015/4 -</SectionSubheading>
+      </Section>
+    </Article>
   </Layout>
 )
 
